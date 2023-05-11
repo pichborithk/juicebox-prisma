02bbf4c3c478c31dc54c2dd16302f8ba93b58933
Pichborith Kong <pichborith@gmail.com>
2023-05-11 11:37:03 -0700

    add user method for database

---
+++ db/dummy.data.ts
@@ -0,0 +1,39 @@
+export const userList = [
+  {
+    username: 'albert',
+    password: 'bertie99',
+    name: 'Al Bert',
+    location: 'Sidney, Australia',
+  },
+  {
+    username: 'sandra',
+    password: '2sandy4me',
+    name: 'Just Sandra',
+    location: 'Chamois, Italy',
+  },
+  {
+    username: 'killer',
+    password: 'k1l3',
+    name: 'kyle',
+    location: 'Florida, US',
+  },
+];
+
+export const postList = [
+  {
+    title: 'First Post',
+    content:
+      'This is my first post. I hope I love writing blogs as much as I love writing them.',
+    tags: ['#happy', '#you-can-do-anything'],
+  },
+  {
+    title: 'How does this work?',
+    content: 'Seriously, does this even do anything?',
+    tags: ['#happy', '#worst-day-ever'],
+  },
+  {
+    title: 'Living the Glam Life',
+    content: 'Do you even? I swear that half of you are posing.',
+    tags: ['#happy', '#you-can-do-anything', '#can-man-do-everything'],
+  },
+];
---
+++ db/user.ts
@@ -0,0 +1,51 @@
+import { db } from '.';
+
+type UserData = {
+  username: string;
+  password: string;
+  name: string;
+  location: string;
+};
+
+export async function createUser(data: UserData) {
+  const user = await db.user.create({
+    data: data,
+  });
+
+  return user;
+}
+
+export async function getAllUsers() {
+  const users = await db.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      location: true,
+      active: true,
+    },
+  });
+
+  return users;
+}
+
+export async function getUserById(userId: number) {
+  const user = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  return user;
+}
+
+export async function updateUser(userId: number, data: UserData) {
+  const user = await db.user.update({
+    where: {
+      id: userId,
+    },
+    data: data,
+  });
+
+  return user;
+}
